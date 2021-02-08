386335a087df3d3a5c927d4335fc0f507a863d0a
EricMTodd <eterictodd@gmail.com>
2021-02-08 13:10:46 -0700

    List correctly appending and displaying.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,41 @@
-console.log("Suh");
+let myLibrary = [
+  {
+    id: 1,
+    title: "The Last Days of Socrates",
+    author: "Plato",
+    genre: "Philosophy"
+  },
+  {
+    id: 2,
+    title: "Republic",
+    author: "Plato",
+    genre: "Philosophy"
+  },
+  {
+    id: 3,
+    title: "Meditations",
+    author: "Marcus Aurelius",
+    genre: "Philosophy"
+  }
+];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+function displayCatalogue() {
+  let catalogue = document.getElementById("catalogue");
+  let catalogueList = document.createElement("ul");
+  catalogue.appendChild(catalogueList);
+  for (let i = 0; i < myLibrary.length; i++) {
+    let newLineItem = document.createElement("li");
+    newLineItem.innerHTML = `Title: ${myLibrary[i].title}`;
+    catalogueList.appendChild(newLineItem);
+  }
+}
+
+displayCatalogue();
