6f8bd14b85249da848edfaddef17c8a97250f3a1
EricMTodd <eterictodd@gmail.com>
2021-02-09 12:24:06 -0700

    Some basic styling in place.

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,21 @@
 let myLibrary = [
   {
     id: 1,
+    artwork: "https://images-na.ssl-images-amazon.com/images/I/81qjQRVKc5L.jpg",
     title: "The Last Days of Socrates",
     author: "Plato",
     genre: "Philosophy"
   },
   {
     id: 2,
-    title: "Republic",
+    artwork: "https://m.media-amazon.com/images/I/51N+DNCnhaL.jpg",
+    title: "The Republic",
     author: "Plato",
     genre: "Philosophy"
   },
   {
     id: 3,
+    artwork: "https://images-na.ssl-images-amazon.com/images/I/51cQEdN9KuL._SX331_BO1,204,203,200_.jpg",
     title: "Meditations",
     author: "Marcus Aurelius",
     genre: "Philosophy"
@@ -32,8 +35,26 @@ function displayCatalogue() {
   let catalogueList = document.createElement("ul");
   catalogue.appendChild(catalogueList);
   for (let i = 0; i < myLibrary.length; i++) {
+    let currentIndex = myLibrary[i]
     let newLineItem = document.createElement("li");
-    newLineItem.innerHTML = `Title: ${myLibrary[i].title}`;
+
+    // HTML
+    newLineItem.innerHTML = `
+    <div class="card">
+      <img src=${currentIndex.artwork} alt="Artwork for ${currentIndex.title}">
+      <div class="container">
+        <p>
+          Title: ${currentIndex.title}
+          <br>
+          Author: ${currentIndex.author}
+          <br>
+          Genre: ${currentIndex.genre}
+        </p>
+      </div>
+    </div>
+    `;
+    // End HTML
+
     catalogueList.appendChild(newLineItem);
   }
 }
