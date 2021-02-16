04668789638b2105692a5653593878863f61e6c4
EricMTodd <eterictodd@gmail.com>
2021-02-16 02:49:43 -0700

    Rewriting HTML and JavaScript to make a better user experience.

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,10 @@
-
-let library = [
-  {
-    id: 1,
-    artwork: "https://images-na.ssl-images-amazon.com/images/I/81qjQRVKc5L.jpg",
-    title: "The Last Days of Socrates",
-    author: "Plato",
-    genre: "Philosophy"
-  },
-  {
-    id: 2,
-    artwork: "https://m.media-amazon.com/images/I/51N+DNCnhaL.jpg",
-    title: "The Republic",
-    author: "Plato",
-    genre: "Philosophy"
-  },
-  {
-    id: 3,
-    artwork: "https://images-na.ssl-images-amazon.com/images/I/51cQEdN9KuL._SX331_BO1,204,203,200_.jpg",
-    title: "Meditations",
-    author: "Marcus Aurelius",
-    genre: "Philosophy"
-  }
-];
+// Global variables
+let initialVisit = true;
+let library = [];
 
 
-// Constructor
-function Book(id, artwork, title, author, genre) {
+// Constructors
+function book(id, artwork, title, author, genre) {
   this.id = id;
   this.artwork = artwork;
   this.title = title;
@@ -35,163 +14,42 @@ function Book(id, artwork, title, author, genre) {
 
 
 // Functions
-function stringifyStorageData() {
-  localStorage.setItem("library", JSON.stringify(library));
-  // console.log(localStorage);
-}
-
-
-function parseStorageData() {
-  library = JSON.parse(localStorage.getItem("library"))
-  // console.log(JSON.parse(localStorage.getItem("library")))
-}
-
-
-let uniqueId = 3;
-function addBookToLibrary() {
-  if (
-  document.getElementById("artwork").value == "" || 
-  document.getElementById("title").value == "" || 
-  document.getElementById("author").value == "" || 
-  document.getElementById("genre").value == "" 
-  ) {
-    alert("Please fill out the form competely.")
+function init() {
+  // Check to see if localStorage has anything in it.
+  if (initialVisit == true) {
+    // Load up dummy data of a few books
+    library = [
+      {
+        id: 1,
+        artwork: "https://images-na.ssl-images-amazon.com/images/I/81qjQRVKc5L.jpg",
+        title: "The Last Days of Socrates",
+        author: "Plato",
+        genre: "Philosophy"
+      },
+      {
+        id: 2,
+        artwork: "https://m.media-amazon.com/images/I/51N+DNCnhaL.jpg",
+        title: "The Republic",
+        author: "Plato",
+        genre: "Philosophy"
+      },
+      {
+        id: 3,
+        artwork: "https://images-na.ssl-images-amazon.com/images/I/51cQEdN9KuL._SX331_BO1,204,203,200_.jpg",
+        title: "Meditations",
+        author: "Marcus Aurelius",
+        genre: "Philosophy"
+      }
+    ]
   } else {
-    uniqueId++;
-    let newBook = new Book(
-      uniqueId,
-      document.getElementById("artwork").value,
-      document.getElementById("title").value,
-      document.getElementById("author").value,
-      document.getElementById("genre").value,
-    );
-    parseStorageData();
-    library.push(newBook);
-    let catalogueList = document.getElementById("catalogueList")
-    let newLineItem = document.createElement("li");
-  
-    newLineItem.innerHTML = `
-    <div id="${newBook.id}" class="card">
-      <img src=${newBook.artwork} alt="Artwork for ${newBook.title}">
-      <div class="container">
-        <p>
-          Title: ${newBook.title}
-          <br>
-          Author: ${newBook.author}
-          <br>
-          Genre: ${newBook.genre}
-        </p>
-      </div>
-      <a id="remove-book-from-library" onclick="removeBookFromLibrary(${newBook.id})">Remove Book</a>
-    </div>
-    `;
-    catalogueList.appendChild(newLineItem);
-  
-    document.getElementById("artwork").value = ""
-    document.getElementById("title").value = ""
-    document.getElementById("author").value = ""
-    document.getElementById("genre").value = ""
-  }
-  stringifyStorageData();
-}
-
-
-function removeBookFromLibrary(target) {
-  for (let i = 0; i < library.length; i++) {
-    if (library[i].id === target) {
-      parseStorageData();
-      library.splice(i, 1)
-    }
-  }
-  document.getElementById(target).remove();
-  stringifyStorageData();
-}
-
-
-let formStatus = false;
-function toggleForm() {
-  let dropdown = document.getElementById("dropdown");
-  let addNewBookButton = document.getElementById("add-new-book-button");
-  if (formStatus == false) {
-    formStatus = true;
-    addNewBookButton.innerHTML = "Hide New Book Form";
-    dropdown.innerHTML = `
-    <div>
-      <fieldset>
-        <legend>Enter Book Details</legend>
-        <input autocomplete="off" placeholder="Artwork URL" type="text" id="artwork" name="artwork"><br><br>
-        <input autocomplete="off" placeholder="Title" type="text" id="title" name="title"><br><br>
-        <input autocomplete="off" placeholder="Author" type="text" id="author" name="author"><br><br>
-        <input autocomplete="off" placeholder="Genre" type="text" id="genre" name="genre"><br><br>
-        <button onclick="addBookToLibrary()">Add Book to Library</button>
-      </fieldset>
-
-    </div>
-    `
-  } else {
-    formStatus = false;
-    addNewBookButton.innerHTML = "Add New Book";
-    dropdown.innerHTML = "";
+    // Load up localStorage
   }
 }
 
-
-function displayCatalogue() {
-  parseStorageData();
-  let catalogueList = document.getElementById("catalogueList")
-  for (let i = 0; i < library.length; i++) {
-    let currentIndex = library[i]
-    let newLineItem = document.createElement("li");
-
-    newLineItem.innerHTML = `
-    <div id="${currentIndex.id}" class="card">
-      <img src=${currentIndex.artwork} alt="Artwork for ${currentIndex.title}">
-      <div class="container">
-        <p>
-          Title: ${currentIndex.title}
-          <br>
-          Author: ${currentIndex.author}
-          <br>
-          Genre: ${currentIndex.genre}
-        </p>
-      </div>
-      <a id="remove-book-from-library" onclick="removeBookFromLibrary(${currentIndex.id})">Remove Book</a>
-    </div>
-    `;
-
-    catalogueList.appendChild(newLineItem);
-
-  }
-  stringifyStorageData();
+function addNewBook() {
+  // Add a book!
 }
 
-function initialize() {
-  parseStorageData();
-  library = [
-    {
-      id: 1,
-      artwork: "https://images-na.ssl-images-amazon.com/images/I/81qjQRVKc5L.jpg",
-      title: "The Last Days of Socrates",
-      author: "Plato",
-      genre: "Philosophy"
-    },
-    {
-      id: 2,
-      artwork: "https://m.media-amazon.com/images/I/51N+DNCnhaL.jpg",
-      title: "The Republic",
-      author: "Plato",
-      genre: "Philosophy"
-    },
-    {
-      id: 3,
-      artwork: "https://images-na.ssl-images-amazon.com/images/I/51cQEdN9KuL._SX331_BO1,204,203,200_.jpg",
-      title: "Meditations",
-      author: "Marcus Aurelius",
-      genre: "Philosophy"
-    }
-  ];
-  location.reload();
-  stringifyStorageData();
+function removeBook() {
+  // Remove the selcted book
 }
-
-displayCatalogue();
